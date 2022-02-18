55804d28068b77cccdad6e4e104ecff9bcdb95d8
Abir Hasan <abirhasan89@gmail.com>
2022-02-19 01:42:08 +0600

    Local DB Classes Setup

---
+++ app/src/main/java/net/omobio/shoppingcarttesting/data/local/ShoppingDao.kt
@@ -0,0 +1,20 @@
+package net.omobio.shoppingcarttesting.data.local
+
+import androidx.room.*
+import kotlinx.coroutines.flow.StateFlow
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    @Delete
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+    @Query("SELECT * FROM SHOPPING_ITEMS")
+    fun observeAllShoppingItems(): StateFlow<List<ShoppingItem>>
+
+    @Query("SElECT SUM(price * amount) from shopping_items")
+    fun observeTotalPrice():StateFlow<Float>
+}
